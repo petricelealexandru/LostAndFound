3a2366963e479dae29f0fddf171f236cd0c643ee
DESKTOP-Alex <petricelealexandru@yahoo.com>
2023-06-13 01:18:20 +0300

    javascript-lost

--- LostAndFound/wwwroot/js/site.js
+++ LostAndFound/wwwroot/js/site.js
@@ -1,24 +1,12 @@
-﻿function createItem() {
-    debugger
-
-    var name = $("#text-id")[0].value;
-
-    var postModel = {
-        TypeId: "FF9E110D-68F8-435C-A082-EFD709FE1A66",
-        Name: name,
-
-    }
-
-    var url = "api/Item/CreateItem";
-
-    ajaxHelper.post(url, postModel,
-        function (result) {
-            debugger
-        },
-        function (err) {
-            debugger
-        });
-}
+﻿var categoriesAvailableList = [
+    {
+        "Id": "86758d30-37f5-43df-acc4-5f7e7632478c",
+        "Text": "Obiect"
+    },
+    {
+        "Id": "e6d73039-ca93-4a10-a856-7c3fe5147b03",
+        "Text": "Animal"
+    }]
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -45,3 +33,44 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+ko.bindingHandlers.datepicker = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        //initialize datepicker with some optional options
+        var options = {
+            format: 'DD/MM/YYYY HH:mm',
+            defaultDate: valueAccessor()()
+        };
+
+        if (allBindingsAccessor() !== undefined) {
+            if (allBindingsAccessor().datepickerOptions !== undefined) {
+                options.format = allBindingsAccessor().datepickerOptions.format !== undefined ? allBindingsAccessor().datepickerOptions.format : options.format;
+            }
+        }
+
+        $(element).datetimepicker(options);
+        var picker = $(element).data('datetimepicker');
+
+        //when a user changes the date, update the view model
+        ko.utils.registerEventHandler(element, "dp.change", function (event) {
+            var value = valueAccessor();
+            if (ko.isObservable(value)) {
+                value(event.date);
+            }
+        });
+
+        var defaultVal = $(element).val();
+        var value = valueAccessor();
+        value(moment(defaultVal, options.format));
+    },
+    update: function (element, valueAccessor) {
+        var widget = $(element).data("datepicker");
+        //when the view model is updated, update the widget
+        if (widget) {
+            widget.date = ko.utils.unwrapObservable(valueAccessor());
+            if (widget.date) {
+                widget.setValue();
+            }
+        }
+    }
+};
